8e302317d0831f39564972f2a002dbda94cb653e
luis3 <luis3@LAPTOP-I5BIBL0E>
2020-04-17 22:43:59 -0500

    practica 4

---
+++ src/Mno/ConversionNumeros.java
@@ -0,0 +1,12 @@
+package Mno;
+
+public class ConversionNumeros {
+
+    
+    public static void main(String[] args) {
+        FrmPrincipal frmPrincipal = new FrmPrincipal();
+        frmPrincipal.setVisible(true);
+        
+    }
+    
+}
